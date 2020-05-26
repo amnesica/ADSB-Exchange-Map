42aa2e5b70c453eed875783a2832ae910ab5cb92
amnesica <amnesica@amnesica.com>
2020-05-26 13:51:37 +0200

    Initial commit

---
+++ app/src/main/java/com/amnesica/adsb_exchange_map/FullscreenActivity.kt
@@ -0,0 +1,171 @@
+package com.amnesica.adsb_exchange_map
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.KeyEvent
+import android.view.View
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.core.content.ContextCompat.startActivity
+import kotlinx.android.synthetic.main.activity_fullscreen.*
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+class FullscreenActivity : AppCompatActivity() {
+    private val url = "https://tar1090.adsbexchange.com/"
+    private var myWebView : WebView? = null
+    private val mHideHandler = Handler()
+    private val mHidePart2Runnable = Runnable {
+        // Delayed removal of status and navigation bar
+
+        // Note that some of these constants are new as of API 16 (Jelly Bean)
+        // and API 19 (KitKat). It is safe to use them, as they are inlined
+        // at compile-time and do nothing on earlier devices.
+        webview.systemUiVisibility =
+                View.SYSTEM_UI_FLAG_LOW_PROFILE or
+                        View.SYSTEM_UI_FLAG_FULLSCREEN or
+                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
+                        View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                        View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+    }
+    private val mShowPart2Runnable = Runnable {
+        // Delayed display of UI elements
+        supportActionBar?.show()
+        fullscreen_content_controls.visibility = View.VISIBLE
+    }
+    private var mVisible: Boolean = false
+    private val mHideRunnable = Runnable { hide() }
+
+    /**
+     * Touch listener to use for in-layout UI controls to delay hiding the
+     * system UI. This is to prevent the jarring behavior of controls going away
+     * while interacting with activity UI.
+     */
+    private val mDelayHideTouchListener = View.OnTouchListener { _, _ ->
+        if (AUTO_HIDE) {
+            delayedHide(AUTO_HIDE_DELAY_MILLIS)
+        }
+        false
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_fullscreen)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        mVisible = true
+
+        // Set up the user interaction to manually show or hide the system UI.
+        webview.setOnClickListener { toggle() }
+
+        myWebView = findViewById(R.id.webview)
+        myWebView?.settings?.javaScriptEnabled = true
+        myWebView?.settings?.domStorageEnabled = true
+        myWebView?.settings?.builtInZoomControls = true
+        myWebView?.settings?.displayZoomControls = false
+        myWebView?.loadUrl(url)
+        myWebView?.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+                if (Uri.parse(url).host == "tar1090.adsbexchange.com") {
+                    // This is my web site, so do not override; let my WebView load the page
+                    view?.loadUrl(url)
+                }
+                // Otherwise, the link is not for a page on my site, so launch another Activity that handles URLs
+                Intent(Intent.ACTION_VIEW, Uri.parse(url)).apply {
+                    startActivity(this)
+                }
+                return true
+            }
+        }
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        // Check if the key event was the Back button and if there's history
+        if (keyCode == KeyEvent.KEYCODE_BACK && myWebView?.canGoBack()!!) {
+            myWebView?.goBack()
+            return true
+        }
+        // If it wasn't the Back key or there's no web page history, bubble up to the default
+        // system behavior (probably exit the activity)
+        return super.onKeyDown(keyCode, event)
+
+    }
+
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+
+        // Trigger the initial hide() shortly after the activity has been
+        // created, to briefly hint to the user that UI controls
+        // are available.
+        delayedHide(100)
+    }
+
+    private fun toggle() {
+        if (mVisible) {
+            hide()
+        } else {
+            show()
+        }
+    }
+
+    private fun hide() {
+        // Hide UI first
+        supportActionBar?.hide()
+        fullscreen_content_controls.visibility = View.GONE
+        mVisible = false
+
+        // Schedule a runnable to remove the status and navigation bar after a delay
+        mHideHandler.removeCallbacks(mShowPart2Runnable)
+        mHideHandler.postDelayed(mHidePart2Runnable, UI_ANIMATION_DELAY.toLong())
+    }
+
+    private fun show() {
+        // Show the system bar
+        webview.systemUiVisibility =
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                        View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+        mVisible = true
+
+        // Schedule a runnable to display UI elements after a delay
+        mHideHandler.removeCallbacks(mHidePart2Runnable)
+        mHideHandler.postDelayed(mShowPart2Runnable, UI_ANIMATION_DELAY.toLong())
+    }
+
+    /**
+     * Schedules a call to hide() in [delayMillis], canceling any
+     * previously scheduled calls.
+     */
+    private fun delayedHide(delayMillis: Int) {
+        mHideHandler.removeCallbacks(mHideRunnable)
+        mHideHandler.postDelayed(mHideRunnable, delayMillis.toLong())
+    }
+
+    companion object {
+        /**
+         * Whether or not the system UI should be auto-hidden after
+         * [AUTO_HIDE_DELAY_MILLIS] milliseconds.
+         */
+        private val AUTO_HIDE = true
+
+        /**
+         * If [AUTO_HIDE] is set, the number of milliseconds to wait after
+         * user interaction before hiding the system UI.
+         */
+        private val AUTO_HIDE_DELAY_MILLIS = 3000
+
+        /**
+         * Some older devices needs a small delay between UI widget updates
+         * and a change of the status and navigation bar.
+         */
+        private val UI_ANIMATION_DELAY = 300
+    }
+}
